529c8c4c8ba0068ec1265623134ddf8bb603be31
Leonardo Guedes <leocguedex@gmail.com>
2022-06-28 21:25:23 -0300

    First Commit

---
+++ src/main/java/leoguedex/com/github/ProjetoVendasAPIEstudos/ProjetoVendasApiEstudosApplication.java
@@ -0,0 +1,14 @@
+package leoguedex.com.github.ProjetoVendasAPIEstudos;
+
+import leoguedex.com.github.ProjetoVendasAPIEstudos.domain.entity.Client;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVendasApiEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVendasApiEstudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leoguedex/com/github/ProjetoVendasAPIEstudos/ProjetoVendasApiEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package leoguedex.com.github.ProjetoVendasAPIEstudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVendasApiEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
